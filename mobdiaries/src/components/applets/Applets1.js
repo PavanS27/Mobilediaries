13f19d97f4b621ea9c0ee934cc533d31775b39bd
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-09-07 12:54:37 +0530

    fin

--- mobdiaries/src/components/applets/Applets1.js
+++ mobdiaries/src/components/applets/Applets1.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import styled from "styled-components";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import FaceIcon from "@material-ui/icons/Face";
 
